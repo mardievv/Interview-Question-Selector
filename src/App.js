73d04ced7801864473d5f5cd28ed599ea30e5176
shakhzodmardiev <mardiev.dev@gmail.com>
2023-04-01 14:49:21 -0400

    input changed to textarea

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css";
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+  const [selectedQuestion, setSelectedQuestion] = useState("");
+
+  function handleAddQuestion(event) {
+    event.preventDefault();
+    const inputString = event.target.elements.question.value;
+    const newQuestions = inputString.split(",");
+    setQuestions(prevQuestions => [...prevQuestions, ...newQuestions]);
+    event.target.reset();
+  }
+
+  function handleSelectQuestion() {
+    const randomIndex = Math.floor(Math.random() * questions.length);
+    const randomQuestion = questions[randomIndex];
+    setSelectedQuestion(randomQuestion);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Interview Question Selector</h1>
+      <form onSubmit={handleAddQuestion}>
+        <label htmlFor="question">Add questions (separated by commas):</label>
+        <textarea id="question" name="question" required></textarea>
+        <button type="submit">Add</button>
+      </form>
+      {questions.length > 0 &&
+        <ul>
+          {questions.map((question, index) => (
+            <li key={index}>{question}</li>
+          ))}
+        </ul>
+      }
+      <button onClick={handleSelectQuestion} disabled={questions.length === 0}>
+        Select a Question
+      </button>
+      {selectedQuestion && <h2>{selectedQuestion}</h2>}
     </div>
   );
 }
